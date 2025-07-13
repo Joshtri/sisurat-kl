95536c011f1b61ce50e00399db58d294eb35b69f
Joshtri <stuffofyos1516@gmail.com>
2025-07-13 18:03:46 +0700

    refactor: update userService to use Users interface and simplify getUsers response

--- services/userService.ts
+++ services/userService.ts
@@ -1,26 +1,22 @@
 // services/user.service.ts
 import axios from "axios";
-import { User } from "@prisma/client";
 
-export async function getUsers(): Promise<User[]> {
+import { Users } from "@/interfaces/users";
+// import { User } from "@prisma/client";
+
+export async function getUsers(): Promise<Users[]> {
   const res = await axios.get("/api/users");
 
-  return res.data.map((u: any) => ({
-    id: u.id,
-    username: u.username,
-    email: u.email ?? undefined, // ✅ ubah null → undefined
-    role: u.role,
-    createdAt: u.createdAt,
-  }));
+  return res.data;
 }
 
-export async function createUser(data: Partial<User>) {
+export async function createUser(data: Partial<Users>) {
   const res = await axios.post("/api/users", data);
 
   return res.data;
 }
 
-export async function getUserById(id: string): Promise<User> {
+export async function getUserById(id: string): Promise<Users> {
   const res = await axios.get(`/api/users/${id}`);
 
   return res.data;
@@ -28,4 +24,10 @@ export async function getUserById(id: string): Promise<User> {
 
 export async function deleteUser(id: string): Promise<void> {
   await axios.delete(`/api/users/${id}`);
+}
+
+export async function updateUser(data: Partial<Users> & { id: string }) {
+  const res = await axios.patch(`/api/users/${data.id}`, data);
+
+  return res.data;
 }
