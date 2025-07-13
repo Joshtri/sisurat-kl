284783c709722e833e8ffcc6060335a53e157e25
Joshtri <stuffofyos1516@gmail.com>
2025-07-13 22:58:16 +0700

    feat: update login route to validate user profiles and improve error messages

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -1,5 +1,4 @@
 import { NextRequest, NextResponse } from "next/server";
-
 import { prisma } from "@/lib/prisma";
 import {
   verifyPassword,
@@ -13,31 +12,47 @@ export async function POST(req: NextRequest) {
 
   if (!nik || !password) {
     return NextResponse.json(
-      { message: "NIK dan password wajib diisi" },
+      { message: "NIK / Email dan password wajib diisi" },
       { status: 400 },
     );
   }
 
   try {
-    const warga = await prisma.warga.findUnique({
-      where: { nik },
-      include: { user: true },
+    let user = await prisma.user.findFirst({
+      where: {
+        OR: [{ email: nik }, { profil: { nik } }, { RTProfile: { nik } }],
+      },
+      include: {
+        profil: true,
+        RTProfile: true,
+      },
     });
 
-    if (!warga || !warga.user) {
+    if (!user) {
       return NextResponse.json(
-        {
-          message: "NIK belum terdaftar. Silakan hubungi pihak administrator.",
-        },
+        { message: "Akun tidak ditemukan. Silakan periksa kembali." },
         { status: 401 },
       );
     }
 
-    const user = warga.user;
     const isValid = await verifyPassword(password, user.password);
-
     if (!isValid) {
       return NextResponse.json({ message: "Password salah." }, { status: 401 });
+    }
+
+    // Validasi profil sesuai role
+    if (user.role === "WARGA" && !user.profil) {
+      return NextResponse.json(
+        { message: "Akun warga belum memiliki profil lengkap." },
+        { status: 401 },
+      );
+    }
+
+    if (user.role === "RT" && !user.RTProfile) {
+      return NextResponse.json(
+        { message: "Akun RT belum memiliki profil RT." },
+        { status: 401 },
+      );
     }
 
     const token = generateToken(user);
@@ -51,7 +66,6 @@ export async function POST(req: NextRequest) {
     });
   } catch (error) {
     console.error("Login error:", error);
-
     return NextResponse.json(
       { message: "Terjadi kesalahan pada server" },
       { status: 500 },
