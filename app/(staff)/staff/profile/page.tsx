d8e4e8d173fda6165c3e1114a35b37929c3052b4
Joshtri <stuffofyos1516@gmail.com>
2025-07-13 22:57:14 +0700

    feat: implement ProfilePage with PageHeader and ProfileGrid components

--- app/(staff)/staff/profile/page.tsx
+++ app/(staff)/staff/profile/page.tsx
@@ -1,5 +1,18 @@
+import { PageHeader } from "@/components/common/PageHeader";
+import ProfileGrid from "@/components/Profile/ProfileGrid";
 import React from "react";
 
 export default function ProfilePage() {
-  return <div>ProfilePage</div>;
+  return (
+    <>
+      <PageHeader
+        breadcrumbs={[
+          { label: "Dashboard", href: "/staff/dashboard" },
+          { label: "Profil" },
+        ]}
+        title="Profil Staff"
+      />
+      <ProfileGrid />
+    </>
+  );
 }
