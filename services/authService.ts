6c48e0124c3673e96801ae0bbe9a1702b6bc3e47
Joshtri <stuffofyos1516@gmail.com>
2025-07-13 18:03:04 +0700

    feat: implement authService for user authentication and session management

---
+++ services/authService.ts
@@ -0,0 +1,35 @@
+import axios from "axios";
+
+interface LoginPayload {
+  nik: string;
+  password: string;
+}
+
+interface LoginResponse {
+  message: string;
+  token: string;
+  role: string;
+  redirect: string;
+  userId?: string;
+}
+
+export async function login(payload: LoginPayload): Promise<LoginResponse> {
+  const res = await axios.post("/api/auth/login", payload);
+
+  return res.data;
+}
+export async function getMe() {
+  const token = localStorage.getItem("token"); // ambil token
+  const res = await axios.get("/api/me", {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  return res.data;
+}
+
+export async function logout(): Promise<void> {
+  await axios.post("/api/auth/logout");
+  localStorage.removeItem("token");
+}
