6256d29d9ed01f406ea82dd507299d010aeb0f15
Joshtri <stuffofyos1516@gmail.com>
2025-07-13 18:03:26 +0700

    feat: update KartuKeluarga service to include additional API methods

--- services/kartuKeluargaService.ts
+++ services/kartuKeluargaService.ts
@@ -1,9 +1,31 @@
 // services/kartuKeluarga.service.ts
 import axios from "axios";
-import { KartuKeluarga } from "@prisma/client";
+
+import { KartuKeluarga } from "@/interfaces/kartu-keluarga";
+// import { KartuKeluarga } from "@prisma/client";
 
 export async function getKartuKeluarga(): Promise<KartuKeluarga[]> {
   const res = await axios.get("/api/kartu-keluarga");
+
+  return res.data;
+}
+
+export async function createKartuKeluarga(data: Partial<KartuKeluarga>) {
+  const res = await axios.post("/api/kartu-keluarga", data);
+
+  return res.data;
+}
+
+export async function getKartuKeluargaById(id: string): Promise<KartuKeluarga> {
+  const res = await axios.get(`/api/kartu-keluarga/${id}`);
+
+  return res.data;
+}
+
+export async function getKartuKeluargaAnggota(
+  id: string,
+): Promise<KartuKeluarga> {
+  const res = await axios.get(`/api/kartu-keluarga/${id}/anggota`);
 
   return res.data;
 }
