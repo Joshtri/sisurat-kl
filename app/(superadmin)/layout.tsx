608a12629b66aac0342f7c611e2d0c68f01621e8
Joshtri <stuffofyos1516@gmail.com>
2025-07-13 17:54:53 +0700

    refactor: simplify SuperAdmin layout by using SuperAdminLayoutClient component

--- app/(superadmin)/layout.tsx
+++ app/(superadmin)/layout.tsx
@@ -1,31 +1,5 @@
-import "@/styles/globals.css";
-import Header from "@/components/partials/Header";
-import Footer from "@/components/partials/Footer";
-import Sidebar from "@/components/partials/Sidebar";
+import SuperAdminLayoutClient from "./SuperAdminLayoutClient";
 
-export default function SuperAdminLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <div className="relative flex min-h-screen bg-gray-50">
-      {/* Sidebar */}
-      <div className="hidden lg:block">
-        <Sidebar userRole="SUPERADMIN" />
-      </div>
-
-      {/* Main Layout */}
-      <div className="flex-1 flex flex-col">
-        <Header userName="Super Admin" userRole="SUPERADMIN" />
-
-        {/* Main Content */}
-        <main className="flex-1 p-6 overflow-y-auto">
-          <div className="max-w-7xl mx-auto">{children}</div>
-        </main>
-
-        <Footer userRole="SUPERADMIN" />
-      </div>
-    </div>
-  );
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return <SuperAdminLayoutClient>{children}</SuperAdminLayoutClient>;
 }
