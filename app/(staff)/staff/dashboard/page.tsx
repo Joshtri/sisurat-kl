d2309f70347a31aa2267eb8c1f45855db78da7c0
Joshtri <stuffofyos1516@gmail.com>
2025-07-18 10:31:36 +0700

    fix: remove unused imports in StaffDashboard component

--- app/(staff)/staff/dashboard/page.tsx
+++ app/(staff)/staff/dashboard/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import DashboardGrid from "@/components/Dashboard/DashboardGrid";
-import { Card, CardBody, CardHeader } from "@heroui/react";
 
 export default function StaffDashboard() {
   return <DashboardGrid />;
